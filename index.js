58b7091273fd0c56c53bf8f5f1da449ab5da66a9
Maciej Małecki <me@mmalecki.com>
2014-05-04 02:31:01 +0200

    The comment was moved

--- index.js
+++ index.js
@@ -8,20 +8,6 @@ var fs = require('fs')
 var CHECK_INTERVAL = 1000
 
 module.exports = function(uuid, callback) {
-  // You thought that waiting for a zone to provision was as simple as waiting
-  // for `vmadm create` to exit? Nope.
-  // As it turns out, each zone after being provisioned by `vmadm` and `vmadmd`
-  // is set to be "finalized" by `zoneinit`: https://github.com/joyent/zoneinit
-  // The worst thing about `zoneinit` is that it reboots the zone when it's
-  // done. So if you open a `zlogin` shell to a zone which has been just
-  // `vmadm create`'d, you're going to be logged out very fast.
-  //
-  // This is a huge problem from the point of view of automation.
-  // This module was created to prevent failures like that by checking for
-  // existance of `/tmp/.FIRST_REBOOT_NOT_COMPLETE_YET`:
-  // https://github.com/joyent/zoneinit/blob/8eedecc075dd4cb8d1ec240cb3e5f741604e1cca/includes/01-reboot-file.sh#L4,
-  // in addition to verifying zone's state with `vmadm`.
-
   setTimeout(function() {
     var succeeded = 0
 
