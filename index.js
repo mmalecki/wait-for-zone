a3879a2bca5fdc60ed81b22a0cb83735031d4ecb
Maciej Małecki <me@mmalecki.com>
2014-05-04 02:38:01 +0200

    Pass uuid back to the callback

--- index.js
+++ index.js
@@ -8,12 +8,13 @@ var fs = require('fs')
 var CHECK_INTERVAL = 1000
 
 module.exports = function(uuid, callback) {
-  setTimeout(function() {
+  setInterval(function() {
     var succeeded = 0
 
-    function checkDone(result) {
+    function checkDone(err, result) {
+      if (err) return callback(err)
       if (result) ++succeeded
-      if (succeeded === 2) return callback(null, true)
+      if (succeeded === 2) return callback(null, uuid)
     }
 
     var child = spawn('vmadm', ['get', uuid])
@@ -24,18 +25,18 @@ module.exports = function(uuid, callback) {
         parsed = JSON.parse(data)
       }
       catch (ex) {
-        return callback(ex)
+        return checkDone(ex)
       }
 
-      checkDone(parsed.zone_state === 'running')
+      checkDone(null, parsed.zone_state === 'running')
     }))
 
 
     fs.stat
       ( path.join('/zones', uuid, 'root', 'tmp', '.FIRST_REBOOT_NOT_COMPLETE_YET')
       , function(err, stat) {
-          if (err && err.code !== 'ENOENT') return callback(err)
-          checkDone(err.code !== 'ENOENT')
+          if (err && err.code !== 'ENOENT') return checkDone(err)
+          checkDone(null, err.code !== 'ENOENT')
         }
       )
 
